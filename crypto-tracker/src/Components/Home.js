af29985f9973e4726d893ffc547eefd83b8e2ac3
Dattunaik5252 <gugulothdattu000@gmail.com>
2022-05-01 01:28:39 +0530

    axios installed to fetch data from api's

--- crypto-tracker/src/Components/Home.js
+++ crypto-tracker/src/Components/Home.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [search, setSearch] = React.useState("");
+  const handler = (e) => {
+    setSearch(e.target.value);
+  };
+  React.useEffect(() => {
+    axios
+      .get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10")
+      .then((res) => console.log(res.data.coins));
+  });
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="header">
+      <input
+        value={search}
+        onChange={handler}
+        type="text"
+        placeholder="search here"
+      />
     </div>
   );
 };
