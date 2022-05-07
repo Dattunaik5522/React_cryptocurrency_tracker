6b5da84a0056c0c5d5f175e459981231a67b3b4b
Dattunaik5252 <gugulothdattu000@gmail.com>
2022-05-08 01:54:45 +0530

    Done

--- crypto-tracker/src/Components/Home.js
+++ crypto-tracker/src/Components/Home.js
@@ -1,26 +1,51 @@
-import React, { useEffect } from "react";
+import React from "react";
+// import { useEffect } from "react";
 import axios from "axios";
+import ImgCard from "./ImgCard";
 
 const Home = () => {
   const [search, setSearch] = React.useState("");
+  const [data, setData] = React.useState([]);
   const handler = (e) => {
     setSearch(e.target.value);
   };
   React.useEffect(() => {
     axios
-      .get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10")
-      .then((res) => console.log(res.data.coins));
-  });
+      .get("https://api.coinstats.app/public/v1/coins?skip=0&limit=25")
+      .then((res) => setData(res.data.coins));
+  }, []);
 
   return (
-    <div className="header">
-      <input
-        value={search}
-        onChange={handler}
-        type="text"
-        placeholder="search here"
-      />
-    </div>
+    <>
+      <div className="header">
+        <input
+          value={search}
+          onChange={handler}
+          type="text"
+          placeholder="search here"
+        />
+      </div>
+      {data.length > 0 && (
+        <div className="grid-container">
+          {data
+            .filter((crypto) =>
+              crypto.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((crypto) => (
+              <div>
+                <ImgCard
+                  name={crypto.name}
+                  rank={crypto.rank}
+                  price={crypto.price}
+                  marketCap={crypto.marketCap}
+                  key={crypto.id}
+                  url={crypto.icon}
+                />
+              </div>
+            ))}
+        </div>
+      )}
+    </>
   );
 };
 
